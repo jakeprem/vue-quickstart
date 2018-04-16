f2365dda590d7c818a12284b1512f52c1408767a
Jake Prem <jprem@hmbnet.com>
2018-04-16 14:31:58 -0400

    Add Vuepress configuration

---
+++ .vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Vue Quickstart',
+  base: '/vue-quickstart/',
+  dest: 'docs',
+  themeConfig: {
+    sidebar: [
+      '/',
+      '/vue-basics',
+      '/vue-components',
+      '/vue-medium',
+      '/es6',
+      '/references'
+    ]
+  }
+}
